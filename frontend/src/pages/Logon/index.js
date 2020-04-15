14fccb69c401f9dd1614f43f33ffe18b79d48834
Guilherme Noe <guinoesil@hotmail.com>
2020-04-15 08:39:58 -0300

    Pagina lista de casos(profile) criada

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./styles.css";
+import { Link } from "react-router-dom";
 import { FiLogIn } from "react-icons/fi";
 import logoimg from "../../assets/logo.svg";
 import heroesimg from "../../assets/heroes.png";
@@ -13,12 +14,15 @@ export default function Logon() {
           <h1>Faça seu logon</h1>
 
           <input placeholder="Sua ID" />
-          <button className="button" type="submit"> Entrar </button>
+          <button className="button" type="submit">
+            {" "}
+            Entrar{" "}
+          </button>
 
-          <a href="register">
-            <FiLogIn size={16} color="#E02041" /> 
+          <Link className='back-link' to="register">
+            <FiLogIn size={16} color="#E02041" />
             Não tenho cadastro
-          </a>
+          </Link>
         </form>
       </section>
 
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import logon from './pages/Logon';
-import Register from './pages/Register';
+import register from './pages/Register';
+import profile from './pages/Profile'
 
 export default function Routes() {
     return(
         <BrowserRouter>
             <Switch>
                 <Route path='/' exact component={logon} />
-                <Route path='./register' component={Register} />
+                <Route path='/register' component={register} />
+                <Route path='/profile' component={profile} />
             </Switch>
         </BrowserRouter>
     )
 }
+
